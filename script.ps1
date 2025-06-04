33d8b40a3d899172671c66b20f05e3769275ba55
fivance <fran.ivancevic7@gmail.com>
2025-06-04 12:10:48 +0200

    Create script.ps1
    auto download/extract sublime config for current user

---
+++ script.ps1
@@ -0,0 +1,19 @@
+$currentUser = $env:USERNAME
+$zipUrl = "https://github.com/fivance/sublime/raw/main/Sublime Text.zip"
+$zipPath = "$env:TEMP\Sublime Text.zip"
+$destinationPath = "C:\Users\$currentUser\AppData\Roaming\Sublime Text"
+
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+
+if (!(Test-Path -Path $destinationPath)) {
+    Write-Host "Destination folder not found: $destinationPath"
+    exit 1
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $destinationPath)
+
+Remove-Item -Path $zipPath -Force
+
+Read-Host "Done: Sublime Text.zip extracted and configuration applied."
+
